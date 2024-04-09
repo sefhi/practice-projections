06fcbd85372ef7d2954d19544438e716adc0481e
Jose Ma Alias <josemariaalias@ipglobal.es>
2024-04-09 08:46:14 +0200

    Feat: add index unique email

---
+++ migrations/Version20240409064439.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240409064439 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add unique index to rrss_user.email';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_908B8D86E7927C74 ON rrss_user (email)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX UNIQ_908B8D86E7927C74');
+    }
+}
